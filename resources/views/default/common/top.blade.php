a6fa93b4a120cbdf2479ad700233a80adb9d9ae6
yejiancong <15900135@qq.com>
2017-05-08 10:53:12 +0800

    add themes

--- resources/views/default/common/top.blade.php
+++ resources/views/default/common/top.blade.php
@@ -1,6 +1,6 @@
 <div id="header">
-    <img class="left" src="/upload/month_201609/2g.jpg" />
-    <img id="shadow" src="/upload/month_201609/shadow.gif" />
-    <h1>Fasan Lab</h1>
-    <img class="right" src="/upload/month_201609/fig2.jpg" />
+    <img class="left" src="/img/top-left.jpg" />
+    <img id="shadow" src="/img/top-shadow.gif" />
+    <h1>Doctliu Lab</h1>
+    <img class="right" src="/img/top-right.jpg" />
 </div>
